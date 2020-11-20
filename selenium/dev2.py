250a8d59e4b54932ad3856d426bc9af98502499d
QuocAnh171 <74640069+QuocAnh171@users.noreply.github.com>
2020-11-20 14:48:34 +0700

    Add files via upload

---
+++ selenium/dev2.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import geckodriver_autoinstaller
+from selenium.webdriver.common.keys import Keys
+
+geckodriver_autoinstaller.install()
+url = "http://45.79.43.178/source_carts/wordpress/wp-admin"
+driver = webdriver.Firefox()
+driver.get(url)
+
+username = driver.find_element_by_name('log')
+username.send_keys('admin')
+password = driver.find_element_by_name('pwd')
+password.send_keys('123456aA')
+password.send_keys(Keys.RETURN)
+
+name = driver.find_element_by_class_name("dislay_name")
+print(name.text)
+
