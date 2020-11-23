b4c941612369ab01644d04bed5bb9aeb48af24a5
QuocAnh171 <74640069+QuocAnh171@users.noreply.github.com>
2020-11-23 11:07:24 +0700

    Update dev2.py

--- selenium/dev2.py
+++ selenium/dev2.py
@@ -1,21 +1,31 @@
 from selenium import webdriver
-import geckodriver_autoinstaller
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+from selenium.webdriver.remote import errorhandler
 
-geckodriver_autoinstaller.install()
-url = "http://45.79.43.178/source_carts/wordpress/wp-admin"
-driver = webdriver.Firefox()
-driver.get(url)
+# geckodriver_autoinstaller.install()
+get_data = webdriver.Chrome()
+get_data.get("http://45.79.43.178/source_carts/wordpress/wp-admin/")
 
-username = driver.find_element_by_name('log')
+username = get_data.find_element_by_name('log')
+username.clear()
 username.send_keys('admin')
-password = driver.find_element_by_name('pwd')
+password = get_data.find_element_by_name('pwd')
+password.clear()
 password.send_keys('123456aA')
 password.send_keys(Keys.RETURN)
 
+sleep(10)
+print(get_data.page_source)
+while True:
+    try:
+        ten = get_data.find_element_by_class_name('display-name')
+        print(ten.text)
+        break
+    except errorhandler.NoSuchElementException:
+        continue
 sleep(20)
+get_data.close()
 
-name = driver.find_element_by_class_name("dislay-name")
-print(name.text)
+
 
